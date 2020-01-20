cbff9586409e1d5b75355f586913dcdf8e96d9a3
sapta94 <dey7.kol@gmail..com>
2020-01-20 23:12:06 +0530

    chnaging sendmail api

--- index.js
+++ index.js
@@ -3,40 +3,51 @@ var app = express(); //init Express
 var bodyParser = require('body-parser');
 var router = express.Router();
 var mailer = require('nodemailer');
+const cors = require('cors');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
+app.options('*', cors());
+
+
+
 
 router.post('/sendMail',function(req,res){
     var userName = req.body.userName;
     var password = req.body.password;
     var content = req.body.content;
-    var sendTo = req.body.sendTo;
+    var sender = req.body.sender;
+    var mobile = req.body.mobile;
 
     let transporter = mailer.createTransport({
     host: 'smtp.rediffmail.com', //your mails smtp
     port: 465,
     secure: true, // secure:true for port 465, secure:false for port 587
     auth: {
-        user: '***your mail-id***',
-        pass: 'password'
+        user: process.env.userName,
+        pass: process.env.password
         }
     });
 
     let mailOptions = {
-        from: '"your_name" <saptarshidey.2009@rediffmail.com>', // sender address
-        to: 'To address', // list of receivers
-        subject: 'Hello ', // Subject line
-        text: 'Hello world ?', // plain text body
-        html: '<b>Hello world ?</b>' // html body
+        from: '"SD" <saptarshidey.2009@rediffmail.com>', // sender address
+        to: 'dey7.kol@gmail.com', // list of receivers
+        subject: 'Message from portfolio ', // Subject line
+        text: 'From: '+sender+'\n Mobile: '+mobile+'\n'+content, // plain text body
     };
+
+    console.log(mailOptions)
 
     // send mail with defined transport object
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             return console.log(error);
         }
+        res.json({
+            message:"success"
+        })
         console.log('Message %s sent: %s', info.messageId, info.response);
     });
 
